f6f2c0cb925da287461f963ea4da7f0b897340bb
nimrodkruger <nimrodkruger@gmail.com>
2024-11-06 20:27:09 +1100

    testing commit and push

--- PERFORMANCE_METRICS/metrics_calc_functions/calc_RSNR.m
+++ PERFORMANCE_METRICS/metrics_calc_functions/calc_RSNR.m
@@ -19,4 +19,23 @@ RateImage_BG_med = cellfun(@(x)check_med(abs(x),1),RateImage_BG);
     end
 
 RSNR = max(RateImage_Sig_med(:))/mean(RateImage_BG_med(:));
+
+
+%% Calculate the correlation between the spatial temporal excitation function of the signal and the backgound function
+% calculate the total rate power 
+Sig_image_intensity = cellfun(@(x)calc_energy(x),RateImage_Sig);
+Bg_image_intensity = cellfun(@(x)calc_energy(x),RateImage_BG);
+% Bg_image_intensity = cellfun(@(x)(abs(x(1))/2+sum(abs(0.5*(x(1:(end-1)).^2-sign(x(1:(end-1)).*x(2:end)).*x(2:end).^2)./(x(2:end).*(x(1:(end-1))-x(2:end)))))^2),RateImage_BG);
+
+RSNR = sum(Sig_image_intensity(:),"omitnan")./median(Bg_image_intensity(:),"omitnan");
+
+    function energy = calc_energy(rate_vec)
+        if length(rate_vec)>1
+            energy = abs(rate_vec(1))/2;
+            energy = energy + sum(abs(0.5*(rate_vec(1:(end-1)).^2-sign(rate_vec(1:(end-1)).*rate_vec(2:end)).*rate_vec(2:end).^2)./(rate_vec(2:end).*(rate_vec(1:(end-1))-rate_vec(2:end)))));
+            % energy = energy^2;
+        else
+            energy = 0;
+        end
+    end
 end
