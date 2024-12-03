dc50d2902de49472d45e7ea4b3764d26384edbad
nimrodkruger <nimrodkruger@gmail.com>
2024-12-03 17:26:55 +1100

    Full analysis MATLAB version updates

--- FullTestAnalysis.m
+++ FullTestAnalysis.m
@@ -30,9 +30,18 @@ addpath("PERFORMANCE_METRICS\metrics_calc_functions\")
 % addpath("..\event_stream\matlab\")
 
 FullTestName = 'frequency_amplitude_heatmap';
-
-Tests = dir(['OUTPUT\',FullTestName,'*']);
-epochs = 1;
+vec = {'4','8','12','16','20','24'};
+Tests = struct('name','','folder','','date','','bytes',0,'isdir',0,'datenum',0);
+ci = 1;
+for vi =1:length(vec)
+    if ~isempty(dir(['OUTPUT\',FullTestName,'*_',vec{vi},'*']))
+        Tests(ci) = dir(['OUTPUT\',FullTestName,'*_',vec{vi},'*']);
+        ci = ci+1;
+    end
+end
+% FullTestName = 'T1';
+% Tests = dir(['OUTPUT\',FullTestName,'*']);
+epochs = 5;
 
 % Inisilized full run metrics maps
 BW_SNR_all = cell(length(Tests),1);
@@ -47,7 +56,7 @@ for ti = 1:length(Tests)
     config_data_file = dir(['OUTPUT\',Tests(ti).name,'\*as_run.ini']);
     [test_data,sanned_param] = readINI([config_data_file.folder,'\',config_data_file.name]);
 
-    if strcmp(test_data.InitParams.sensor_model,'manual')
+    if strcmp(test_data.InitParams.sensor_model,'Manual')
         matrix_size = [test_data.ManualSensorParams.width ,test_data.ManualSensorParams.height];
     elseif strcmp(test_data.InitParams.sensor_model,'Gen4')
         Gen4_config = readINI('config\Gen4_config.ini');
@@ -73,6 +82,7 @@ for ti = 1:length(Tests)
 
 
         for vi = 1:length(vector)
+            n_ep = 0;
             for ep = 1:epochs
                 if ~mod(vector(vi),1)
                     add_0 = '.0';
@@ -84,12 +94,19 @@ for ti = 1:length(Tests)
                 ev_file_name = ['OUTPUT\',Tests(ti).name,'\events_and_labels\ev_',test_data.InitParams.sim_name,'_',sanned_param{2},'_',num2str(vector(vi)),add_0,'_ep',num2str(ep),'.txt'];
                 simdata_file_name = ['OUTPUT\',Tests(ti).name,'\events_and_labels\simdata_',test_data.InitParams.sim_name,'_',sanned_param{2},'_',num2str(vector(vi)),add_0,'_ep',num2str(ep),'.mat'];
                 if isfile(ev_file_name) && isfile(simdata_file_name)
-                    event_data = load(ev_file_name);
-                    load(simdata_file_name);
+                    try
+                        event_data = load(ev_file_name);
+                        load(simdata_file_name);
+                    catch
+                        warning(['Corrupt simulation result files: ',ev_file_name]);
+                        continue
+                    end
                 else
-                    error(['Missing simulation result files: ',ev_file_name]);
+                    warning(['Missing simulation result files: ',ev_file_name]);
+                    continue
                 end
 
+                n_ep = n_ep+1;
 
                 dt = simulation_data{2}.t - simulation_data{1}.t;
                 T = simulation_data{end-1}.t;
@@ -143,13 +160,13 @@ for ti = 1:length(Tests)
                 inds = ~isnan(mean_Signal_event_rate);
                 bw_snr = sum(abs(mean_Signal_event_rate(inds)-mean_BG_event_rate(inds)));
                 disp([' - BW_SNR = ',num2str(bw_snr)]);
-                BW_SNR(vi) = BW_SNR(vi) + bw_snr/epochs; % average value
+                BW_SNR(vi) = BW_SNR(vi) + bw_snr; % average value
                 % Add some sort of dysplay here for debuging
 
                 % Calculate Rate SNR metric - TO FIX. Not a good metric...
                 [rsnr, RateImage_Sig_med, RateImage_BG_med]= calc_RSNR(signal_rate_stack(:,:,1),bg_rate_stack(:,:,1),matrix_size);
                 disp([' - Rate SNR = ', num2str(rsnr)])
-                RSNR(vi) = RSNR(vi) + rsnr/epochs;
+                RSNR(vi) = RSNR(vi) + rsnr;
                 % figure;
                 % subplot(2,2,1)
                 % imagesc(log(RateImage_Sig_med+1)'); colorbar ;
@@ -165,7 +182,7 @@ for ti = 1:length(Tests)
                 % Calculate Rate SNR metric for aligned event cloud - TO FIX. Not a good metric...
                 [al_rsnr, RateImage_Sig_aligned_med, RateImage_BG_aligned_med]= calc_RSNR(signal_rate_aligned_stack(:,:,1),bg_rate_aligned_stack(:,:,1),matrix_size);
                 disp([' - aligned Rate SNR = ', num2str(al_rsnr)])
-                Al_RSNR(vi) = Al_RSNR(vi) + al_rsnr/epochs;
+                Al_RSNR(vi) = Al_RSNR(vi) + al_rsnr;
 
                 % subplot(2,2,3)
                 % imagesc(log(RateImage_Sig_aligned_med+1)'); colorbar ;
@@ -177,24 +194,24 @@ for ti = 1:length(Tests)
             end
         end
 
-        BW_SNR_all{ti} = BW_SNR;
-        RSNR_all{ti} = RSNR;
-        Al_RSNR_all{ti} = Al_RSNR;
+        BW_SNR_all{ti} = BW_SNR/n_ep;
+        RSNR_all{ti} = RSNR/n_ep;
+        Al_RSNR_all{ti} = Al_RSNR/n_ep;
 
         figure(1)
-        plot(vector,BW_SNR); hold on; grid on;
+        loglog(vector,BW_SNR_all{ti}); hold on; grid on;
         leg = [leg,{replace(Tests(ti).name,'_',' ')}];
-        xlabel(sanned_param{2});
+        xlabel(replace(sanned_param{2},'_',' '));
         ylabel('BW_S_N_R [Hz]')
 
         figure(2)
-        plot(vector,RSNR); hold on; grid on;
-        xlabel(sanned_param{2});
+        loglog(vector,RSNR_all{ti}); hold on; grid on;
+        xlabel(replace(sanned_param{2},'_',' '));
         ylabel('Rate SNR')
 
         figure(3)
-        plot(vector,Al_RSNR); hold on; grid on;
-        xlabel(sanned_param{2});
+        loglog(vector,Al_RSNR_all{ti}); hold on; grid on;
+        xlabel(replace(sanned_param{2},'_',' '));
         ylabel('Aligned Rate SNR')
         drawnow
         
@@ -202,7 +219,12 @@ for ti = 1:length(Tests)
         disp(' ');
     end
 end
-
+figure(1)
+legend(leg)
+figure(2)
+legend(leg)
+figure(3)
+legend(leg)
 % figure;
 % plot(val-max(val)/2,RSNR); hold on
 % plot(val-max(val)/2,Al_RSNR); legend('not aligned RateSNR','aligned RateSNR');
